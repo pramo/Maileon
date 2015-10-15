bd93ed32a7168012120d8156125ab69963d86592
viktor <balogh.viktor@maileon.hu>
2015-10-15 15:24:31 +0200

    Added method to return actual enum value

--- Maileon/Mailings/MailingField.cs
+++ Maileon/Mailings/MailingField.cs
@@ -17,6 +17,22 @@ namespace Maileon.Mailings
         [XmlElement("value")]
         public string Value { get; set; }
 
+        public object GetValue()
+        {
+            switch (Name)
+            {
+                case MailingFieldNames.Type:
+                    return MaileonEnums.FromXmlString<MailingTypes>(Value);
+                case MailingFieldNames.ScheduleTime:
+                    return new Timestamp(Value);
+                case MailingFieldNames.State:
+                    return MaileonEnums.FromXmlString<MailingStates>(Value);
+                default:
+                    return Value;
+            }
+        }
+
+
         public MailingField() { }
     }
 }
