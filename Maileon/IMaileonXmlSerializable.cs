e574289ef193605a6c7f8cbebc5731d12edbccc8
viktor <balogh.viktor@maileon.hu>
2015-10-15 11:21:45 +0200

    Deleted junk projects

---
+++ Maileon/IMaileonXmlSerializable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml;
+
+namespace Maileon
+{
+    interface IMaileonXmlSerializable<T>
+    {
+        T ReadXml(XmlReader reader);
+        void WriteXml(XmlWriter writer, T obj);
+    }
+}
