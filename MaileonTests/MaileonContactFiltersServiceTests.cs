4e54d94746f63b239509c2f8670d6a1d30ec7cb4
Viktor Balog <balogh.viktor@maileon.hu>
2020-03-20 11:04:32 +0100

    use the settings for properties

--- MaileonTests/MaileonContactFiltersServiceTests.cs
+++ MaileonTests/MaileonContactFiltersServiceTests.cs
@@ -10,19 +10,15 @@ namespace MaileonTests
     {
         protected MaileonContactFiltersService service;
 
-        protected const string BASE_URI = "https://api.maileon.com/1.0";
-        protected const string API_KEY = "260afd12-3b6a-40de-910c-b52102e280b1";
-        protected const long EXISTING_CONTACT_FILTER_ID = 1755;
-
         public MaileonContactFiltersServiceTests()
         {
-            service = new MaileonContactFiltersService(new MaileonConfiguration(API_KEY, BASE_URI, true));
+            service = new MaileonContactFiltersService(new MaileonConfiguration(Properties.Settings.Default.API_KEY, Properties.Settings.Default.BASE_URI, true));
         }
 
         [TestMethod]
         public void TestRefreshContactFilter()
         {
-            service.RefreshContactFilterContacts(EXISTING_CONTACT_FILTER_ID);
+            service.RefreshContactFilterContacts(Properties.Settings.Default.EXISTING_CONTACT_FILTER_ID);
         }
     }
 }
